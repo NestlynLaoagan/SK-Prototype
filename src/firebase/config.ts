94048283b8c0177b3e588cb91e953920757c62b5
Nesh Lhyn <neshlhyn@gmail.com>
2026-01-19 19:01:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4794751776-84c1b",
+  "appId": "1:200075374562:web:89c5a7e99fd47241ba13bb",
+  "apiKey": "AIzaSyBu6KyEDjJUJAd2eFCLuuJCstll8aaFmYo",
+  "authDomain": "studio-4794751776-84c1b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "200075374562"
+};
